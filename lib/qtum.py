3bcf350ae7edaf97ae700098d991d0b48bb6359e
TT-Rong <rongtuotuo@yeah.net>
2018-08-07 09:23:49 +0800

    fix send token decimal and sign bug

--- lib/qtum.py
+++ lib/qtum.py
@@ -120,7 +120,8 @@ def witness_push(item: str) -> str:
 
 
 def op_push(i: int) -> str:
-    if i < 0x4c:  # OP_PUSHDATA1
+    #if i < 0x4c:  # OP_PUSHDATA1
+    if i < 0xa0:
         return int_to_hex(i)
     elif i <= 0xff:
         return '4c' + int_to_hex(i)
